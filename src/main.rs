8521d359514af6c22e6565193604d0c25fc09ca8
Fluturecode <fluturecode@gmail.com>
2022-10-07 19:50:14 -0400

    added std library to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-
+use std::io;
 
 fn console() {
+    println!("\nLet's have fun with Bitcoin!\n")
 
+    println!("Do you want to send (s) or receive (r) Bitcoin?\n");
+                // struct::method
+    let mut command = String::new();
+
+    io::stdin().read_line(&mut command)
 }
 
 
