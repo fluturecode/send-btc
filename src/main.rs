12485be12be71ab98eaf1496503483f550327c1f
Fluturecode <fluturecode@gmail.com>
2022-10-11 19:50:30 -0400

    fixed lint errors

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,26 @@ fn send_bitcoin() {
 
 }
 
+fn recieve_bitcoin() {
+
+}
+
+fn exit_console() {
+    print!("Invalid option, must be (s) or (r)");
+}
+
 fn console() {
-    println!("\nLet's have fun with Bitcoin!\n")
+    println!("\nLet's have fun with Bitcoin!\n");
 
     println!("Do you want to send (s) or receive (r) Bitcoin?\n");
                 // struct::method
     let mut command = String::new();
 
-    io::stdin().read_line(&mut command)
+    io::stdin().read_line(&mut command);
 
-    if command.trim().eq('s') {
+    if command.trim().eq("s") {
         send_bitcoin()
-    } else if command.trim().eq('r) {
+    } else if command.trim().eq("r") {
         recieve_bitcoin()
     } else {
         exit_console()
